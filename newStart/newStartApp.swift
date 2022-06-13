33660a2ffecf303d2233ccc19668aeb3db0c2957
justintheduffer <106621717+justintheduffer@users.noreply.github.com>
2022-06-13 11:34:27 -0700

    Initial Commit

---
+++ newStart/newStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  newStartApp.swift
+//  newStart
+//
+//  Created by Justin N on 6/13/22.
+//
+
+import SwiftUI
+
+@main
+struct newStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
